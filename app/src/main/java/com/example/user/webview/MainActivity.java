155bf12f0811cd1ed724c3d18dc64c33a4c2a113
Jocelin Thomas <jocelinthomas177@gmail.com>
2018-04-03 12:55:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/webview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = (WebView) findViewById(R.id.web);
+        wv.setWebViewClient(new MyBrowser());
+        wv.getSettings().setLoadsImagesAutomatically(true);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("http://www.google.com");
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
